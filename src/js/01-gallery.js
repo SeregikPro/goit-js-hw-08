1bf1cc93160df691890215356dfdfa060d5f826b
Sergii Proiava <seregikpro@gmail.com>
2022-05-03 22:29:53 +0300

    add Gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,30 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector(".gallery");
+
+const elGalleryMarkup = createElGalleryMarkup(galleryItems);
+
+function createElGalleryMarkup (images) {
+    return images.map(image =>
+        `<a class="gallery__item" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      alt="${image.description}"
+    />
+  </a>
+</div>`).join(''); 
+};
+
+gallery.innerHTML = elGalleryMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', 
+{docClose: false,
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+});
